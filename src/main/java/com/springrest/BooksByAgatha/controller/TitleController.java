07fcedd6f509bafed15d95452c7dd9044158d8db
nandy91 <nandynew@gmail.com>
2020-04-23 14:10:28 -0400

    Adding Swagger for Api Documentation

---
+++ src/main/java/com/springrest/BooksByAgatha/controller/TitleController.java
@@ -0,0 +1,12 @@
+package com.springrest.BooksByAgatha.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@Api(value="Title API")
+public class TitleController {
+
+}
