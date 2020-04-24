788fa9c7128a735b6aee47e058409f2ec52ab7a1
nandy91 <nandynew@gmail.com>
2020-04-24 13:44:15 -0400

    modify swagger to springdoc and swaggerui

--- src/main/java/com/springrest/BooksByAgatha/controller/TitleController.java
+++ src/main/java/com/springrest/BooksByAgatha/controller/TitleController.java
@@ -1,12 +1,51 @@
 package com.springrest.BooksByAgatha.controller;
 
-import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.springrest.BooksByAgatha.model.Title;
+import com.springrest.BooksByAgatha.service.TitleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.awt.*;
+import java.net.URI;
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/")
-@Api(value="Title API")
+
 public class TitleController {
+    @Autowired
+    private TitleService titleService;
+
+    @PostMapping("/titles")
+    @Operation(description = "Create a new title of Agatha Christie", responses = {
+            @ApiResponse(content = @Content(schema = @Schema(implementation = Title.class)), responseCode = "201")})
+    public ResponseEntity<Title> newTitle(@RequestBody Title title, UriComponentsBuilder ucb) {
+        Title newTitle = titleService.createTitle(title);
+        URI locationUri =
+                ucb.path("/titles/")
+                        .path(String.valueOf(newTitle.getId()))
+                        .build()
+                        .toUri();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(locationUri);
+        return new ResponseEntity<Title>(newTitle,headers, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/titles")
+    @Operation(description = "Returns all available Titles of Agatha Christie", responses = {
+            @ApiResponse(content = @Content(schema = @Schema(implementation = Title.class)), responseCode = "200")})
+    public ResponseEntity<Iterable<Title>> getTitles() {
+        Iterable<Title> titles = titleService.getAllTitles();
+
+        return new ResponseEntity<Iterable<Title>>(titles,HttpStatus.OK);
+    }
 
 }
--- src/main/java/com/springrest/BooksByAgatha/service/TitleServiceImpl.java
+++ src/main/java/com/springrest/BooksByAgatha/service/TitleServiceImpl.java
@@ -3,20 +3,21 @@ package com.springrest.BooksByAgatha.service;
 import com.springrest.BooksByAgatha.model.Title;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TitleServiceImpl {
 
     //CREATE
-    public Title createTitle();
-
+    public Title createTitle(Title title);
 
     //READ
-    public List<Title> getAllTitles();
-    public List<Title> getAll();
-    public Title getTitle(int id);
+    public Iterable<Title> getAllTitles();
+    //public Iterable<Title> getBySeries(String series);
+    public Optional<Title> getTitle(int id);
 
     //UPDATE
-
+    //public Title updateTitle(Title updatedTitle);
 
     //DELETE
+    //public boolean deleteTitle(int id);
 }
